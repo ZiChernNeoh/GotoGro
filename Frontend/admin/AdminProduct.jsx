1395a878959b35983f6b5af8963e09795c4ef7d4
ZiChernNeoh <76463416+ZiChernNeoh@users.noreply.github.com>
2024-11-03 14:31:27 +0800

    Update AdminProduct.jsx

--- Frontend/admin/AdminProduct.jsx
+++ Frontend/admin/AdminProduct.jsx
@@ -11,7 +11,7 @@ const AdminProduct = () => {
     const fetchProducts = async () => {
         try {
             const response = await axios.get('/admindash/products');
-            console.log(response.data);
+            // console.log(response.data);
             setProducts(response.data);
         } catch (error) {
             console.error('Error fetching products:', error);
